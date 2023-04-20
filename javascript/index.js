46544a103e5a0d648a82b07619c6ca3460429c55
JimmyKurui <jimmy.chepkurui@strathmore.edu>
2023-04-20 03:02:18 +0300

    Remodelling of Hero, Projects and Footer sections. Content mods for About, Services
    Hero image changed. About paragraph added to About me. Service view reduced. Thumbnail carousel working using Splide

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,24 @@
 // Default theme
-import '@splidejs/splide/css';
 
-import Splide from '@splidejs/splide';
+// import Splide from '@splidejs/splide';
+// var main = new Splide('#main-slider', {
+//     type: 'fade',
+//     rewind: true,
+//     pagination: false,
+//     arrows: false,
+// });
 
-new Splide( '.splide' ).mount();
+new Splide('#thumbnail-carousel', {
+    fixedWidth: 100,
+    fixedHeight: 80,
+    gap: 10,
+    rewind: true,
+    pagination: false,
+    isNavigation: true,
+    breakpoints: {
+        600: {
+            fixedWidth: 60,
+            fixedHeight: 44,
+        },
+    },
+}).mount();
