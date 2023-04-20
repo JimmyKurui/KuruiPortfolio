864f9109bc3d420d237d8aa0b4c83462527d68ff
JimmyKurui <jimmy.chepkurui@strathmore.edu>
2023-04-20 15:43:47 +0300

    Navbar with basic dynamic animation

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,27 @@
 // Default theme
 
+// Nav
+
+const navbar = document.querySelector('.navbar');
+
+function toggleClass(element, className) {
+    if (element.classList.contains(className)) {
+        element.classList.remove(className);
+    } else {
+        element.classList.add(className);
+    }
+}
+
+window.addEventListener('scroll', () => {
+
+    if (window.scrollY > 500) {
+        navbar.classList.add('navbar-scroll');
+    } else {
+        navbar.classList.remove('navbar-scroll');
+    }
+});
+// navbar.style.backgroundImage = 'linear-gradient(117deg, rgba(238,238,238,1) 0%, rgba(255,255,255,0.3) 85%, rgba(239,0,255,1) 100%)';
+
 // import Splide from '@splidejs/splide';
 // var main = new Splide('#main-slider', {
 //     type: 'fade',
