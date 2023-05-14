bd116382c59fad2c732c589096ed7dcf82cce93f
JimmyKurui <jimmy.chepkurui@strathmore.edu>
2023-05-14 17:44:20 +0300

    About me section correctly formatted and highlighted. History tree fixed. Services padded

--- javascript/index.js
+++ javascript/index.js
@@ -1,21 +1,14 @@
 // Default theme
 
-// Nav
+// ---- HEADER ----
 window.onload = (event) => {
     setTimeout(() => {
         alert("This is a beta site version. Check it out on desktop for the best experience!")
     }, 200)
 };
+
+// ---- Nav ----
 const navbar = document.querySelector('.navbar');
-
-function toggleClass(element, className) {
-    if (element.classList.contains(className)) {
-        element.classList.remove(className);
-    } else {
-        element.classList.add(className);
-    }
-}
-
 window.addEventListener('scroll', () => {
 
     if (window.scrollY > 500) {
@@ -26,25 +19,38 @@ window.addEventListener('scroll', () => {
 });
 // navbar.style.backgroundImage = 'linear-gradient(117deg, rgba(238,238,238,1) 0%, rgba(255,255,255,0.3) 85%, rgba(239,0,255,1) 100%)';
 
-// import Splide from '@splidejs/splide';
-// var main = new Splide('#main-slider', {
-//     type: 'fade',
-//     rewind: true,
-//     pagination: false,
-//     arrows: false,
-// });
+// ============================= PROJECTS ===========================
+// Glide Carousel
+document.addEventListener('DOMContentLoaded', function() {
+    var main = new Splide('#main-carousel', {
+        type: 'fade',
+        rewind: true,
+        pagination: false,
+        arrows: false,
+    });
 
-new Splide('#thumbnail-carousel', {
-    fixedWidth: 100,
-    fixedHeight: 80,
-    gap: 10,
-    rewind: true,
-    pagination: false,
-    isNavigation: true,
-    breakpoints: {
-        600: {
-            fixedWidth: 60,
-            fixedHeight: 44,
+
+    var thumbnails = new Splide('#thumbnail-carousel', {
+        fixedWidth: 100,
+        fixedHeight: 60,
+        gap: 10,
+        rewind: true,
+        pagination: false,
+        isNavigation: true,
+        breakpoints: {
+            600: {
+                fixedWidth: 60,
+                fixedHeight: 44,
+            },
         },
-    },
-}).mount();
+    });
+
+
+    main.sync(thumbnails);
+    main.mount();
+    thumbnails.mount();
+});
+// Add caption
+carouselSlide = document.querySelector('#main-carousel li.splide__slide');
+carouselCaption = document.createElement('p').textContent = "123";
+carouselSlide.appendChild(carouselCaption);
